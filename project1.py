ad2ac403f3a2e6e659574cb6c6b1b95ac1c73cd7
anildalar <dollorinfotech@gmail.com>
2024-01-02 09:15:44 +0530

    Create Operation Done

--- project1.py
+++ project1.py
@@ -1,5 +1,10 @@
 import sqlite3
+import sys
 
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QVBoxLayout, QPushButton, QMessageBox # * means all module
+
+#         ceo = ClassName()
+#         ceo = module.ClassName()
    # = module.method()
 conn = sqlite3.connect("./mydatabase.sqlite")
 cursor = conn.cursor()
@@ -14,15 +19,65 @@ query = '''
         '''
 cursor.execute(query) # query is an actual argument
 conn.commit()
+app = QApplication([])# [] is a list in python
 
-query = "DELETE FROM employees;"
-#  ceo.method(aa1,aa2)
-#  ceo.method(string,tuple)
-cursor.execute(query)
+# Create widgets
+name_label = QLabel('Name:')
+name = QLineEdit()
 
-query = "INSERT INTO employees (name, position, salary) VALUES (?, ?, ?);"
-#  ceo.method(aa1,aa2)
-#  ceo.method(string,tuple)
-cursor.execute(query, ("Anil", 'Director', 50000.00))
-cursor.execute(query, ("Sunil", 'MD', 70000.50))
-conn.commit()
+position_label = QLabel('Position:')
+position = QLineEdit()
+
+salary_label = QLabel('Salary:')
+salary = QLineEdit()
+
+submit_button = QPushButton('Submit')
+
+#1. Function defination
+def myFunction(): # myFunction is writter in camelCase
+    print("Hi Hello")
+    print(f"name={name.text()} position={position.text()} salary={salary.text()}  ")
+    #query = "DELETE FROM employees;"
+    #  ceo.method(aa1,aa2)
+    #  ceo.method(string,tuple)
+    #cursor.execute(query)
+
+    query = "INSERT INTO employees (name, position, salary) VALUES (?, ?, ?);"
+    #  ceo.method(aa1,aa2)
+    #  ceo.method(string,tuple)
+    cursor.execute(query, (name.text(), position.text(), salary.text()))
+    conn.commit()
+    
+    msgBox = QMessageBox()
+    msgBox.setText("Employee Saved Successfully")
+    msgBox.exec()
+    pass
+
+
+submit_button.clicked.connect(myFunction)#2 i am calling/invoking the function
+
+# Set up layout
+layout = QVBoxLayout()
+layout.addWidget(name_label)
+layout.addWidget(name)
+layout.addWidget(position_label)
+layout.addWidget(position)
+layout.addWidget(salary_label)
+layout.addWidget(salary)
+layout.addWidget(submit_button)
+
+# Set up window
+window = QWidget()
+window.setLayout(layout)
+window.setWindowTitle('Employee Form')
+#window.setGeometry(X,    Y,   W, H  )
+window.setGeometry(500, 300, 300, 200)
+
+# Show the window
+window.show()
+sys.exit(app.exec())
+
+
+
+
+
